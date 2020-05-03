2d099b3764963800cbccae5cf7a0f7e4308abe06
kek <arthur.from.rivia@gmail.com>
2020-05-03 19:56:17 +0200

    Aggiunta classe Progetto più file uploader

--- app/src/main/java/it/unimib/bicap/DettaglioQuestionario.java
+++ app/src/main/java/it/unimib/bicap/DettaglioQuestionario.java
@@ -1,16 +1,37 @@
 package it.unimib.bicap;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 import java.util.Objects;
 
 public class DettaglioQuestionario extends AppCompatActivity {
+
+    private static final int CODE_VIDEO = 1;
+    private static final int CODE_PDF = 2;
+    private StorageReference mStorageRef;
+    private Uri filePath;
+    String type;
+    TextView linkQuestionario;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
@@ -18,11 +39,130 @@ public class DettaglioQuestionario extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dettaglio_questionario);
 
+        linkQuestionario = (TextView) findViewById(R.id.etLink);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
         Toolbar toolbar = findViewById(R.id.toolbar_main);
         toolbar.setTitle("Nome Progetto");
         setSupportActionBar(toolbar);
 
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
+
+        findViewById(R.id.imCaricaVideo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(Intent.createChooser(new Intent()
+                                .setAction(Intent.ACTION_GET_CONTENT)
+                                .setType("video/mp4"),
+                        "Seleziona un video"), CODE_VIDEO);
+            }
+        });
+
+        findViewById(R.id.imInsertPdf).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(Intent.createChooser(new Intent()
+                                .setAction(Intent.ACTION_GET_CONTENT)
+                                .setType("application/pdf"),
+                        "Seleziona un PDF"), CODE_PDF);
+            }
+        });
+
+        findViewById(R.id.imNextStep).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Capire se sto uploadando PDF/Video o se sto inserendo il link del questionario -> aggiungere una XOR (forse)
+                // TODO: per essere sicuri che solo un campo su tre è occupato
+
+                if (type.equals("Video")){
+                    uploadFile("Video");
+                }
+                else if (type.equals("PDF")) {
+                    uploadFile("Documenti");
+                }
+                else {
+                    // TODO: Operazione per il salvataggio del link del questionario nel JSON
+                }
+                filePath = null;
+                linkQuestionario.setText("");
+            }
+        });
+
+        findViewById(R.id.imSaveProject).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Capire se sto uploadando PDF/Video o se sto inserendo il link del questionario
+
+                if (type.equals("Video")){
+                    uploadFile("Video MP4/file");
+                }
+                else if (type.equals("PDF")) {
+                    uploadFile("Documenti PDF");
+                }
+                else {
+                    // TODO: Operazione per il salvataggio del link del questionario nel JSON
+                }
+                // TODO: Final step di creazione JSON -> sovrascrittura del file -> upload del file -> salvataggio nuovo link sul DB
+                // TODO: Reindirizzare l'utente ad un'activity dove ci sarà scritto "Progetto salvato con successo"
+            }
+        });
+
+
+    }
+
+    private void uploadFile (String directory) {
+
+        if (filePath != null) {
+            StorageReference riversRef = mStorageRef.child(directory);
+
+            riversRef.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(getApplicationContext(), "Hai aggiunto un passo", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            Toast.makeText(getApplicationContext(), "Errore nell'upload", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                            updateProgress(taskSnapshot);
+                        }
+                    });
+        } else {
+            Toast.makeText(this, "Non hai scelto nessun file", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void updateProgress(UploadTask.TaskSnapshot taskSnapshot) {
+        long fileSize = taskSnapshot.getTotalByteCount();
+        long uploadBytes = taskSnapshot.getBytesTransferred();
+        long progress = (100 * uploadBytes) / fileSize;
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.pbUpload);
+        progressBar.setProgress((int) progress);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CODE_VIDEO && resultCode == RESULT_OK && data != null && data.getData() != null){
+            this.filePath = data.getData();
+            this.type = "Video";
+            Toast.makeText(getApplicationContext(), "Hai selezionato un video", Toast.LENGTH_SHORT).show();
+        }else if (requestCode == CODE_PDF && resultCode == RESULT_OK && data != null && data.getData() != null){
+            this.filePath = data.getData();
+            this.type = "PDF";
+            Toast.makeText(getApplicationContext(), "Hai selezionato un file PDF", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Non hai selezionato nulla", Toast.LENGTH_SHORT).show();
+        }
     }
 }
