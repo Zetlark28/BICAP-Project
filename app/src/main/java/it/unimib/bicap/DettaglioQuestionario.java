78e3364fc730f88576321ec1239b5a8ba1ffb77c
Alessio Cottarelli <a.cottarelli1@campus.unimib.it>
2020-05-09 15:13:11 +0200

    ""

--- app/src/main/java/it/unimib/bicap/DettaglioQuestionario.java
+++ app/src/main/java/it/unimib/bicap/DettaglioQuestionario.java
@@ -6,7 +6,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
