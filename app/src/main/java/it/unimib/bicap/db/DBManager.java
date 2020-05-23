894dc9d68888b4a8b91fa5edb6189fc8813865fa
Zetlark28 <clarke2897@gmail.com>
2020-05-23 11:11:44 +0200

    creato DB SQLite

---
+++ app/src/main/java/it/unimib/bicap/db/DBManager.java
@@ -0,0 +1,99 @@
+package it.unimib.bicap.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+
+public class DBManager {
+
+    private DBHelper dbhelper;
+    public DBManager(Context ctx) {
+        dbhelper=new DBHelper(ctx);
+    }
+    public void saveCompletati(Integer idUtente, Integer idProgetto)
+    {
+        SQLiteDatabase db=dbhelper.getWritableDatabase();
+        ContentValues cv=new ContentValues();
+        cv.put(DBConstants.FIELD_ID_UTENTE, idUtente);
+        cv.put(DBConstants.FIELD_ID_PROGETTO, idProgetto);
+        try {
+            db.insert(DBConstants.TBL_NAME_COMPLETATI, null,cv);
+        }
+        catch (SQLiteException sqle) {
+
+        }
+    }
+    public void saveDaCompletare(Integer idUtente, Integer idProgetto)
+    {
+        SQLiteDatabase db=dbhelper.getWritableDatabase();
+        ContentValues cv=new ContentValues();
+        cv.put(DBConstants.FIELD_ID_UTENTE, idUtente);
+        cv.put(DBConstants.FIELD_ID_PROGETTO, idProgetto);
+        try
+        {
+            db.insert(DBConstants.TBL_NAME_DA_COMPLETARE, null,cv);
+        }
+        catch (SQLiteException sqle) {
+        }
+    }
+
+    public Cursor selectCompletati(Integer idUtente) {
+        Cursor crs=null;
+        try
+        {
+            SQLiteDatabase db=dbhelper.getReadableDatabase();
+            crs=db.query(DBConstants.TBL_NAME_COMPLETATI, new String[]{DBConstants.FIELD_ID_PROGETTO},DBConstants.FIELD_ID_UTENTE + "=?",
+                    new String[]{Integer.toString(idUtente)}, null, null, null, null);
+        }
+        catch(SQLiteException sqle)
+        {
+            return null;
+        }
+        return crs;
+    }
+
+    public Cursor selectDaCompletare(Integer idUtente) {
+        Cursor crs=null;
+        try
+        {
+            SQLiteDatabase db=dbhelper.getReadableDatabase();
+            crs=db.query(DBConstants.TBL_NAME_COMPLETATI, new String[]{DBConstants.FIELD_ID_PROGETTO},DBConstants.FIELD_ID_UTENTE + "=?",
+                    new String[]{Integer.toString(idUtente)}, null, null, null, null);
+        }
+        catch(SQLiteException sqle)
+        {
+            return null;
+        }
+        return crs;
+    }
+    public boolean deleteCompletati(Integer idUtente, Integer idProgetto)
+    {
+        SQLiteDatabase db=dbhelper.getWritableDatabase();
+        try
+        {
+            if (db.delete(DBConstants.TBL_NAME_COMPLETATI, DBConstants.FIELD_ID_UTENTE+"=? AND " + DBConstants.FIELD_ID_PROGETTO ,
+                    new String[]{Integer.toString(idUtente), Integer.toString(idProgetto)})==1)
+                return true;
+            return false;
+        }
+        catch (SQLiteException sqle) {
+            return false;
+        }
+    }
+    public boolean deleteDaCompletare(Integer idUtente, Integer idProgetto)
+    {
+        SQLiteDatabase db=dbhelper.getWritableDatabase();
+        try
+        {
+            if (db.delete(DBConstants.TBL_NAME_DA_COMPLETARE, DBConstants.FIELD_ID_UTENTE+"=? AND " + DBConstants.FIELD_ID_PROGETTO ,
+                    new String[]{Integer.toString(idUtente), Integer.toString(idProgetto)})==1)
+                return true;
+            return false;
+        }
+        catch (SQLiteException sqle) {
+            return false;
+        }
+    }
+}
