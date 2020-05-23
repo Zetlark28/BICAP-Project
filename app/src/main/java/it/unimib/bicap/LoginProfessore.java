29c05d451e1ed639eae803cbe0f026c78c423a03
Alessio Cottarelli <a.cottarelli1@campus.unimib.it>
2020-05-23 11:39:50 +0200

    Fix Nome Autore

--- app/src/main/java/it/unimib/bicap/LoginProfessore.java
+++ app/src/main/java/it/unimib/bicap/LoginProfessore.java
@@ -1,7 +1,9 @@
 package it.unimib.bicap;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.os.Bundle;
@@ -20,6 +22,11 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import it.unimib.bicap.databinding.ActivityLoginProfessoreBinding;
 
@@ -29,6 +36,8 @@ public class LoginProfessore extends AppCompatActivity {
     private ActivityLoginProfessoreBinding binding;
     private FirebaseAuth mAuth;
     private boolean fromHome;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("utenti");
 
     @Override
     public void onStart() {
@@ -41,11 +50,43 @@ public class LoginProfessore extends AppCompatActivity {
     }
 
     private void updateUI(FirebaseUser currentUser, boolean fromHome) {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        final SharedPreferences sharedPref = getSharedPreferences("author", Context.MODE_PRIVATE);
 
         if (user != null) {
             if (fromHome) {
                 String email = user.getEmail();
+                // Read from the database
+
+                if(sharedPref.getString("autore", null) == null) {
+
+                    Log.d(TAG, "shared preferences non presenti");
+
+                    myRef.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            // This method is called once with the initial value and again
+                            // whenever data at this location is updated.
+                            String autore = dataSnapshot.child(user.getUid()).getValue(Somministratore.class).getAutore();
+
+                            Log.d(TAG, "Value is: " + autore);
+
+                            SharedPreferences.Editor editor = sharedPref.edit();
+
+                            editor.putString("autore", autore);
+                            editor.commit();
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError error) {
+                            // Failed to read value
+                            Log.w(TAG, "Failed to read value.", error.toException());
+                        }
+                    });
+                }
+                else
+                    Log.d(TAG, "shared preferences presenti");
+
                 Intent intentLogged = new Intent(this, HomePageSomministratore.class);
                 intentLogged.putExtra("Email", email);
                 intentLogged.putExtra("fromHome", fromHome);
