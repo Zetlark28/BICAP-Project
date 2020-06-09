13bdc0cf4d64da25fbee2899f8c3f6baa9914b45
Samuele Cogliati <s.cogliati@campus.unimib.it>
2020-06-09 09:49:14 +0200

    Prova ricerca

--- app/src/main/java/it/unimib/bicap/QuestionariDaFare.java
+++ app/src/main/java/it/unimib/bicap/QuestionariDaFare.java
@@ -1,12 +1,19 @@
 package it.unimib.bicap;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.database.Cursor;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
@@ -26,7 +33,8 @@ public class QuestionariDaFare extends Fragment {
     private static JSONArray progetti;
     private DBManager db=null;
     private JSONObject progettiTot;
-
+    private SearchView searchView = null;
+    private SearchView.OnQueryTextListener queryTextListener;
     private ProgettiAdapterRV progettiAdapterRV;
     private String from = "daFare";
 
@@ -71,5 +79,53 @@ public class QuestionariDaFare extends Fragment {
 
 
         return rootView;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_ricerca, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+
+        if (searchItem != null) {
+            searchView = (SearchView) searchItem.getActionView();
+        }
+        if (searchView != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+
+            queryTextListener = new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    Log.i("onQueryTextChange", newText);
+
+                    return true;
+                }
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    Log.i("onQueryTextSubmit", query);
+
+                    return true;
+                }
+            };
+            searchView.setOnQueryTextListener(queryTextListener);
+        }
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+
+                return false;
+            default:
+                break;
+        }
+        searchView.setOnQueryTextListener(queryTextListener);
+        return super.onOptionsItemSelected(item);
     }
 }
