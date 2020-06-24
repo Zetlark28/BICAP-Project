96686df45498403f884ec4f25812d2f873a0b814
Zetlark28 <clarke2897@gmail.com>
2020-06-25 01:11:31 +0200

    refactor

--- app/src/main/java/it/unimib/bicap/constanti/ActivityConstants.java
+++ app/src/main/java/it/unimib/bicap/constanti/ActivityConstants.java
@@ -2,7 +2,6 @@ package it.unimib.bicap.constanti;
 
 public class ActivityConstants {
 
-    public static final String HOMEPAGE_TOOLBAR_TITLE = "SurveyMiB";
     public static final String HOMEPAGE_SOMMINISTRATORE_TOOLBAR_TITLE = "Homepage Somministratore";
     public static final String CREAZIONE_PROGETTO_TOOLBAR_TITLE = "Creazione progetto";
     public static final String ELIMINA_PROGETTI_TOOLBAR_TITLE = "Eliminazione progetto";
@@ -12,7 +11,6 @@ public class ActivityConstants {
     public static final String SHARED_PREFERENCE_AUTORE_KEY = "autore";
 
     public static final String AUTHORIZED_EMAIL = "admin@admin.com";
-    public static final String AUTHORIZED_PASSWORD = "alessio";
 
     public static final String DATABASE_URL="https://bicap-ffecb.firebaseio.com/";
     public static final String DATABASE_API_KEY="AIzaSyArVc7FWmbpTNMh6JbFq69Kimm-TqS5e28";
@@ -35,12 +33,9 @@ public class ActivityConstants {
     public static final String INTENT_RETURN = "return";
     public static final String INTENT_NEW_LIST_AUTORE = "newListAutore";
     public static final String INTENT_NEW_LIST_TOT= "newListTot";
-    public static final String INTENT_NEW_OBJECT = "newObject";
-    public static final String INTENT_HOME ="home";
     public static final String INTENT_SOMMINISTRATORI = "somministratori";
     public static final String INTENT_GUIDE_OR_PDF ="guideOrPDF";
     public static final String INTENT_EMAIL= "email";
-    public static final String INTENT_FROM_HOME ="fromHome";
     public static final String INTENT_LINK_VIDEO = "linkVideo";
 
     public static final String EMAIL_ADMIN = "bicap.project@gmail.com";
