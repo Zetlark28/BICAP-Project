e079990a441427e7f56d55c62257bdd85f5b7eb7
Zetlark28 <clarke2897@gmail.com>
2020-06-24 22:36:42 +0200

    Refactor CreazioneProgetto

--- app/src/main/java/it/unimib/bicap/activity/somministratore/CreazioneProgetto.java
+++ app/src/main/java/it/unimib/bicap/activity/somministratore/CreazioneProgetto.java
@@ -4,35 +4,30 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONObject;
 
-import java.util.Objects;
-
 import it.unimib.bicap.R;
 import it.unimib.bicap.constanti.ActivityConstants;
 import it.unimib.bicap.databinding.ActivityCreazioneProgettoBinding;
-
+import it.unimib.bicap.exception.CreazioneProgettoException;
 import it.unimib.bicap.service.JsonBuilder;
 
 public class CreazioneProgetto extends AppCompatActivity {
 
-    private static final String TAG = "CreazioneProgetto";
     private ActivityCreazioneProgettoBinding binding;
     private static JsonBuilder jsonBuilder = JsonBuilder.getJsonBuilder();
     private String progettiJSON;
     @SuppressLint({"SourceLockedOrientationActivity", "ClickableViewAccessibility"})
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,8 +43,6 @@ public class CreazioneProgetto extends AppCompatActivity {
 
         final String autore = getSharedPreferences(ActivityConstants.SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE).getString(ActivityConstants.SHARED_PREFERENCE_AUTORE_KEY, null);
 
-        Log.d(TAG, "autore: " + autore);
-
         binding.tvAutoreFinale.setText(autore);
 
         //aggiunto metodo navigazione toolbar - elena
@@ -63,18 +56,19 @@ public class CreazioneProgetto extends AppCompatActivity {
             }
         });
 
-        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
+        if(getSupportActionBar()==null){
+            throw CreazioneProgettoException.CREAZIONE_PROGETTO_VIEW_FAIL;
+        }
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         binding.etDescrizione.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (v.getId() == R.id.etDescrizione) {
                     v.getParent().requestDisallowInterceptTouchEvent(true);
-                    switch (event.getAction() & MotionEvent.ACTION_MASK) {
-                        case MotionEvent.ACTION_UP:
-                            v.getParent().requestDisallowInterceptTouchEvent(false);
-                            break;
+                    if ((event.getAction() & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_UP) {
+                        v.getParent().requestDisallowInterceptTouchEvent(false);
                     }
                 }
                 return false;
@@ -85,8 +79,10 @@ public class CreazioneProgetto extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                if(binding.etNomeProgetto.getText() == null || binding.etDescrizione.getText() == null){
+                    throw CreazioneProgettoException.CREAZIONE_PROGETTO_VIEW_FAIL;
+                }
                 String nomeProgetto = binding.etNomeProgetto.getText().toString();
-                //String autoreProgetto = binding.etAutore.getText().toString();
                 String descrizioneProgetto = binding.etDescrizione.getText().toString();
 
                 if (nomeProgetto.equals("")){
@@ -94,9 +90,6 @@ public class CreazioneProgetto extends AppCompatActivity {
                 }
                 else if (nomeProgetto.length()>31)
                     Snackbar.make(v, "Attenzione, non puoi inserire un titolo più lungo di 30 caratteri!", Snackbar.LENGTH_SHORT).show();
-                //else if (autoreProgetto.equals("")){
-                    //Snackbar.make(v, "Attenzione, manca l'autore del progetto !", Snackbar.LENGTH_SHORT).show();
-                //}
                 else if (descrizioneProgetto.equals("")){
                     Snackbar.make(v, "Attenzione, manca la descrizione del progetto!", Snackbar.LENGTH_SHORT).show();
                 }
@@ -106,7 +99,6 @@ public class CreazioneProgetto extends AppCompatActivity {
                 else{
                     JSONObject progetto = jsonBuilder.creaProgetto(nomeProgetto,descrizioneProgetto,autore);
                     Intent intentDettaglioProgetto = new Intent(getApplicationContext(), DettaglioQuestionario.class);
-                    Log.d(TAG, progetto.toString());
                     intentDettaglioProgetto.putExtra(ActivityConstants.INTENT_PROGETTO, progetto.toString());
                     intentDettaglioProgetto.putExtra(ActivityConstants.INTENT_LISTA_PROGETTI, progettiJSON);
                     intentDettaglioProgetto.putExtra(ActivityConstants.INTENT_NOME_PROGETTO, nomeProgetto);
