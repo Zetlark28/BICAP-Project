3490723c07196810b9ad19dacae85b424ead9839
Gaia <gaiacroci98@gmail.com>
2020-06-25 11:27:05 +0200

    refactor

--- app/src/main/java/it/unimib/bicap/activity/utente/GrazieScreen.java
+++ app/src/main/java/it/unimib/bicap/activity/utente/GrazieScreen.java
@@ -21,23 +21,7 @@ public class GrazieScreen extends AppCompatActivity {
         setContentView(R.layout.activity_grazie_screen);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-        /*final int flags;
-
-        int currentAPIVersion = Build.VERSION.SDK_INT;
-
-        if(currentAPIVersion >= Build.VERSION_CODES.KITKAT){
-            flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-            getWindow().getDecorView().setSystemUiVisibility(flags);
-        }else{
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        }*/
-
+        
         handler=new Handler();
         handler.postDelayed(new Runnable() {
             @Override
